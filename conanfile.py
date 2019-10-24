aec686573e4be2b79fc77f7cdd6c872859713392
Simon Dirmeier <simon.dirmeier@web.de>
2019-10-24 13:24:31 +0200

    Add conan file

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,13 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conans import ConanFile, Meson, tools
+import os
 
-
-class HelloConan(ConanFile):
-    name = "cstat"
+class ConanTest(ConanFile):
+    name = "stats"
     version = "0.1"    
+    generators = "pkg_config"
     settings = "os", "compiler", "build_type", "arch"    
-    default_options = {"shared": True}
-    
+        
     def build(self):
-        autotools = AutoToolsBuildEnvironment(self)
-        autotools.configure()
-        autotools.make()
-
-    def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
-
-    def package_info(self):
-        self.cpp_info.libs = ["hello"]
-
+        meson = Meson(self)
+        meson.configure(build_folder="build")
+        meson.build()
